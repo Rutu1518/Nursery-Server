9cffa3d2c2eebeff1af1789cd3663608af2994d2
rutu1518 <rutujamore1508@gmail.com>
2024-07-19 13:55:19 +0530

    controllers and mongodb connection

---
+++ controllers/Health.js
@@ -0,0 +1,7 @@
+function getHealth(req, res){
+    res.json({
+        message: "Server is up and running...",
+        success:true
+    })
+}
+export {getHealth}
--- index.js
+++ index.js
@@ -1,198 +1,70 @@
 import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
+import mongoose from "mongoose";
+
+import { getHealth } from "./controllers/Health.js";
+import {
+     postPlant,
+     getPlants,
+     getPlantId,
+     putPlantId, 
+     deletePlantId  
+    } from "./controllers/Plant.js";
+import { handlePageNotFound } from "./controllers/errors.js";
 
 const app = express()
-
 app.use(express.json())
 
-const plants = [
-      {  
-        "id": 5,
-        "name": "Bamboo",
-        "category": "outdoor",
-        "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
-        "price": 150,
-        "description": "Lucky Bamboo 2 Layer Feng Shui Plant With Acrylic Pot and Stones"   
-     },
-     {  
-        "id": 2,
-        "name": "Rose",
-        "category": "outdoor",
-        "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
-        "price": 200,
-        "description": "Rose Plant With Acrylic Pot and Stones"   
-     },
-     {  
-        "id": 4,
-        "name": "Mango",
-        "category": "outdoor",
-        "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
-        "price": 250,
-        "description": "Mango Plant With Acrylic Pot and Stones"   
-     }
-]
+// const plants = [
+//       {  
+//         "id": 5,
+//         "name": "Bamboo",
+//         "category": "outdoor",
+//         "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
+//         "price": 150,
+//         "description": "Lucky Bamboo 2 Layer Feng Shui Plant With Acrylic Pot and Stones"   
+//      },
+//      {  
+//         "id": 2,
+//         "name": "Rose",
+//         "category": "outdoor",
+//         "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
+//         "price": 200,
+//         "description": "Rose Plant With Acrylic Pot and Stones"   
+//      },
+//      {  
+//         "id": 4,
+//         "name": "Mango",
+//         "category": "outdoor",
+//         "image": "https://unlimitedgreens.com/cdn/shop/files/IMG_4861_768x960_fef97dca-d6a6-4ea7-b708-f996952d60d2.jpg?v=1686312519",
+//         "price": 250,
+//         "description": "Mango Plant With Acrylic Pot and Stones"   
+//      }
+// ]
 
-app.post("/plant", (req, res)=>{
-    const{
-        name,
-        category,
-        image,
-        price,
-        description}=req.body
-      
-        if(!name){
-           return res.json({
-                success:false,
-                data:null,
-                message:"name is required...."
-            })
-        }
+const dbConnection = async () =>{
+  const conn = await mongoose.connect(process.env.MONGO_URL)
 
-        if(!category){
-            return res.json({
-                 success:false,
-                 data:null,
-                 message:"category is required...."
-             })
-         }
+  if(conn){
+    console.log(`MongoDB connected... `)
+  }
+  else{
+    console.log(`MongoDB not connected`)
+  }
+}
+dbConnection();
 
-         if(!image){
-            return res.json({
-                 success:false,
-                 data:null,
-                 message:"image is required...."
-             })
-         }
+app.get("/Health", getHealth)
+app.post("/plant", postPlant)
+app.get("/plants", getPlants)
+app.get("/plant/:id", getPlantId)
+app.put("/plant/:id", putPlantId )
+app.delete("/plant/:id", deletePlantId)
 
-         if(!price){
-            return res.json({
-                 success:false,
-                 data:null,
-                 message:"price is required...."
-             })
-         }
+app.use("*", handlePageNotFound)
 
-         if(!description){
-            return res.json({
-                 success:false,
-                 data:null,
-                 message:"description is required...."
-             })
-         }
-        const randomId = Math.round(Math.random() * 10000)
-
-        const newPlant = {
-            id:randomId,
-            name:name,
-            category: category,
-            image: image,
-            price: price,
-            description: description
-
-        }
-        plants.push(newPlant)
-
-        res.json({
-            success:true,
-            data:newPlant,
-            message: "New plant added successfully"
-        })
-})
-
-app.get("/plants", (req, res)=>{
-    res.json({
-        success:true,
-        data:plants,
-        message: "All plants added successfully"
-    })
-})
-
-app.get("/plant/:id",(req ,res)=>{
-    const{id}=req.params
-  
-     const plant = plants.find((plant)=>plant.id == id)
-    
-    res.json({
-        success:plant ? true : false,
-        data:id,
-        message:plant ? "plant fetched successfully" : "plant not found"
-    })
-})
-
-app.put("/plant/:id", (req, res)=>{
-    const{id}=req.params
-   
-    let index = -1
-
-    plants.forEach((plant, i)=>{
-        if(plant.id == id){
-            index = i
-        }
-    })
- 
-    const newObj = {
-        id,
-        name,
-        category,
-        image,
-        price,
-        description
-
-    }
-    if(index==-1){
-        return res.json({
-            success:false,
-            data:null,
-            message: "plant not found for id ${id}"
-        })
-    }
-    else{
-        plants[index] = newObj        
-        return res.json({
-             success:true,
-             message:"plant updated successfully",
-             data:newObj
-          })
-
-    }
-   
-})
-
-app.delete("/plant/:id",(req, res)=>{
-    const{id}=req.params
-
-     let index = -1
-     plants.forEach((plant, i)=>{
-        if(plant.id == id){
-            index = i
-            }
-        })
-
-        if(index==-1){
-            return res.json({
-                success:true,
-                // data:null,
-                message: `plant not found for id ${id}`
-                })
-        }
-        plants.splice(index, 1)
-
-        res.json({
-             success:true,
-             message:"plant deleted successfully",
-             data:null
-         })
-})
-
-app.use("*", (req, res)=>{
-    res.send(`<div>
-     <h1 style="text-align:center;"> 404 not found</h1>
-    </div> 
-`)
-})
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT|| 8000
 
 app.listen(PORT, ()=>{
     console.log(`server is running on $(PORT)`)
